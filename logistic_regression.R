ce8d43d45a12d41a9b3df3981690264b7c47db30
Nino <ninofoudraine@hotmail.com>
2020-02-20 15:43:20 +0100

    klaaaaaar

--- logistic_regression.R
+++ logistic_regression.R
@@ -11,14 +11,16 @@ library(data.table)
 
 '%ni%' <- Negate('%in%')
 
-itReLS <- function(lambda_vector, train, validation, epsilon, userlist, AE_list_under_threshold){
+itReLS <- function(lambda_vector, train, validation, epsilon, userlist, AE_list_under_threshold, clickrate, AE_clickrate_list_under_threshold){
   AE_folds <- list()
   MAE_folds <- rep(NA, length(train))
   MAE_lambda <- rep(NA, length(lambda_vector))
   MAE_folds_boven_threshold <- rep(NA, length(length(train)))
   MAE_lambda_boven_threshold <- rep(NA, length(lambda_vector))
   # MAE_zero_folds <- rep(NA, length(train))
-  # MAE_clickrate_folds <- rep(NA, length(train))
+  AE_clickrate_folds <- list()
+  MAE_clickrate_folds <- rep(NA, length(train))
+  MAE_clickrate_folds_boven_threshold <- rep(NA, length(train))
   
   for (l in 1:length(lambda_vector)) {
     print(paste0('Lambda ', l))
@@ -36,14 +38,14 @@ itReLS <- function(lambda_vector, train, validation, epsilon, userlist, AE_list_
         # training set
         train_set <- train_kfold[USERID == userlist[j]]
         train_set <- merge(train_set, OfferDetails, by = c("OFFERID", "MAILID"))
-        train_set <- train_set[,-c("OFFERID", "MAILID", "USERID")] #subset(train_set,select = c(CLICK, PRICE, JANUARY:DECEMBER, DISCOUNT_RATE, REVIEW_RATING, STAR_RATING, DURATION, BULGARIA:ISRAEL)) # train_set <- subset(data_complete, select = -c(OFFERID, MAILID, USERID, ROOM_OCCUPANCY, ACCOMMODATION_NAME, USP1, USP2, USP3, DEPARTURE_DATE, ROOMTYPE, MONTH_SUM))
+        train_set <- train_set[,-c("OFFERID", "MAILID", "USERID")] 
         
         # test set
         test_set_complete <- validation_kfold[USERID == userlist[j]]
         test_set <- merge(test_set_complete, OfferDetails, by = c("OFFERID", "MAILID"))
-        test_set <- test_set[,-c("OFFERID", "MAILID", "USERID")]#subset(data_test_complete,select = c(CLICK, PRICE, JANUARY:DECEMBER, DISCOUNT_RATE, REVIEW_RATING, STAR_RATING, DURATION, BULGARIA:ISRAEL)) #test_set <- subset(data_test_complete, select = -c(OFFERID, MAILID, USERID, ROOM_OCCUPANCY, ACCOMMODATION_NAME, USP1, USP2, USP3, DEPARTURE_DATE, ROOMTYPE, MONTH_SUM))
+        test_set <- test_set[,-c("OFFERID", "MAILID", "USERID")]
         
-        if (nrow(test_set) == 0) {
+        if (nrow(test_set) == 0 || nrow(train_set)== 0) {
           # print('no forecasts needed')
           # print(j)
           next
@@ -55,28 +57,25 @@ itReLS <- function(lambda_vector, train, validation, epsilon, userlist, AE_list_
         parm[is.na(parm)] <- 0
         # Parameter estimation with Iteratively Re-weighted Least Squares
         opt_parm <- RidgeRegr(parm, train_set, lambda, epsilon)
-        
-        #print(opt_parm)
 
         # Fit test observations
         prob <- FitRidge(opt_parm, test_set)
         x <- test_set_complete
         x$click_prob <- prob
-        x$AE_clickrate <- abs(x$CLICK - mean(train_set$CLICK))
+        x$AE_clickrate <- abs(x$CLICK - clickrate$x[clickrate$user == userlist[j]])
         total_probs[[j]] <- x
-        
       }
       game_probs <- dplyr::bind_rows(total_probs)
       
-      #AE_folds[[i]] <- abs(game_probs$CLICK - game_probs$click_prob)
+      
       AE_folds[[i]] <- c(abs(game_probs$CLICK - game_probs$click_prob),AE_list_under_threshold[[i]])
-  
       MAE_folds[i] <- mean(AE_folds[[i]])
       MAE_folds_boven_threshold[i] <- mean(abs(game_probs$CLICK - game_probs$click_prob))
       
-#      MAE_zero_folds[i] <- mean(game_probs$CLICK)
-#      click_rate <- mean(train_set$CLICK)
-#      MAE_clickrate_folds[i] <- mean(x$AE_clickrate)
+      #AE_zero_folds[i] <- game_probs$CLICK
+      AE_clickrate_folds[[i]] <- c(game_probs$AE_clickrate, AE_clickrate_list_under_threshold[[i]])
+      MAE_clickrate_folds[i] <- mean(AE_clickrate_list_under_threshold[[i]])
+      MAE_clickrate_folds_boven_threshold[i] <- mean(game_probs$AE_clickrate)
       
       end_time <- Sys.time()
       print(end_time - start_time)
@@ -84,12 +83,14 @@ itReLS <- function(lambda_vector, train, validation, epsilon, userlist, AE_list_
     
     MAE_lambda[l] <- mean(MAE_folds)
     MAE_lambda_boven_threshold[l] <- mean(MAE_folds_boven_threshold)
+    MAE_clickrate <- mean(MAE_clickrate_folds)
+    MAE_clickrate_boven_threshold <- mean(MAE_clickrate_folds_boven_threshold)
   }
   # MAE_zero <- mean(MAE_zero_folds)
   # MAE_clickrate <- mean(MAE_clickrate_folds) 
   
   #return(list(MAE_table = rbind(MAE_lambda, MAE_zero, MAE_clickrate), probs = game_probs))  
-  return(list(MAE_table = rbind(MAE_lambda, MAE_lambda_boven_threshold), probs = game_probs))
+  return(list(MAE_table = rbind(MAE_lambda, MAE_clickrate, MAE_lambda_boven_threshold, MAE_clickrate_boven_threshold), probs = game_probs))
 }
 
 
@@ -143,7 +144,7 @@ Observations = read.csv(file.choose(), header = T, sep = ';',stringsAsFactors = 
 # STAP 1: Delete zero clickers
 clickrate_per_user <- aggregate(Observations$CLICK, by = list(user = Observations$USERID), FUN = mean)
 obs_per_user <- aggregate(Observations$CLICK, by = list(user = Observations$USERID), FUN = length)
-nonzero_clickers <- clickrate_per_user$user[clickrate_per_user$x > 0 & obs_per_user$x > 1]
+nonzero_clickers <- clickrate_per_user$user[clickrate_per_user$x > 0 & obs_per_user$x > 2]
 Observations <- Observations[Observations$USERID %in% nonzero_clickers,]
 
 # STAP 2: Split data in train-test (met alle data)
@@ -156,7 +157,7 @@ testing  <- Observations[-intrain,] # test
 n_folds <- 4
 lambda_vector <- c(0.1, exp(1),  exp(4),  exp(7), exp(10), exp(12), exp(15))
 epsilon <- 10^-4
-threshold <- 0.1 # pas deze aan
+threshold <- 0.5 # pas deze aan
 
 # STAP 4: Split in Repeated Holdout folds
 # clickrate_training <- aggregate(training$CLICK, by = list(user = training$USERID), FUN = mean)
@@ -164,6 +165,7 @@ userlist_threshold <- clickrate_per_user$user[clickrate_per_user$x > threshold]
 train_boven_threshold <- list()
 validation_boven_threshold <- list()
 AE_under_threshold <- list()
+AE_clickrate_under_threshold <- list()
 
 for (i in 1:n_folds){
   set.seed(2*i)
@@ -176,8 +178,12 @@ for (i in 1:n_folds){
   validation_boven_threshold[[i]] <- tussenstap_validation[tussenstap_validation$USERID %in% userlist_threshold,]
   validation_under_threshold <- tussenstap_validation[tussenstap_validation$USERID %ni% userlist_threshold,]
   AE_under_threshold[[i]] <- validation_under_threshold$CLICK
+  validation_under_threshold <- merge(validation_under_threshold,clickrate_per_user, by.x = 'USERID', by.y = 'user')
+  AE_clickrate_under_threshold[[i]] <- abs(validation_under_threshold$CLICK - validation_under_threshold$x)
 }
 
 # STAP 5: Run Algoritme
-userlist_part_1 <- userlist_threshold[1:round(1/100*length(userlist_threshold))]
-results <- itReLS(lambda_vector = lambda_vector, train = train_boven_threshold, validation = validation_boven_threshold, epsilon = epsilon, userlist = userlist_part_1, AE_list_under_threshold = AE_under_threshold)
+userlist_part_1 <- userlist_threshold[1:round(1/1*length(userlist_threshold))]
+results <- itReLS(lambda_vector = lambda_vector, train = train_boven_threshold, validation = validation_boven_threshold, epsilon = epsilon, userlist = userlist_part_1, AE_list_under_threshold = AE_under_threshold, clickrate = clickrate_per_user, AE_clickrate_list_under_threshold = AE_clickrate_under_threshold)
+results$MAE_table
+
